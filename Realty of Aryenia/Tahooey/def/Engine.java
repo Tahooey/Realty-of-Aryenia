a7515d11d83c958ccf4644c67df4f9629abc0b1e
Will <theonlytahooey@hotmail.com>
2012-12-06 19:59:21 +1100

    Engine Change 6/12/12

--- Realty of Aryenia/Tahooey/def/Engine.java
+++ Realty of Aryenia/Tahooey/def/Engine.java
@@ -3,29 +3,31 @@ package def;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
+import mob.Entity;
+
 import wrld.*;
 
 public class Engine {
 	
 	public static WorldFileHandler fh = new WorldFileHandler();
 	public static MapBuilder mb = new MapBuilder();
-	public static Camera cam = new Camera();
+	public static Camera cam = new Camera(400,200);
 	public static void RunGame(Graphics g){
 		WorldRunner.RunWorld(g);
 	}
 	public static void KeyPressed(KeyEvent e){
 		int KeyCode = e.getKeyCode();
 		if(KeyCode==KeyEvent.VK_W){
-			cam.move(Camera.UP);
+			mb.ENTITIES[0].move(Entity.UP);
 		}
 		if(KeyCode==KeyEvent.VK_S){
-			cam.move(Camera.DOWN);
+			mb.ENTITIES[0].move(Entity.DOWN);
 		}
 		if(KeyCode==KeyEvent.VK_A){
-			cam.move(Camera.LEFT);
+			mb.ENTITIES[0].move(Entity.LEFT);
 		}
 		if(KeyCode==KeyEvent.VK_D){
-			cam.move(Camera.RIGHT);
+			mb.ENTITIES[0].move(Entity.RIGHT);
 		}
 	}
 
