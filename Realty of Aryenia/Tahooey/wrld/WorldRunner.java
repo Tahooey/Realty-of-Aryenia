4d4f845afd8a14683cbefa3539a274fa95ca7112
Will <theonlytahooey@hotmail.com>
2012-12-06 20:01:06 +1100

    Running Entities

--- Realty of Aryenia/Tahooey/wrld/WorldRunner.java
+++ Realty of Aryenia/Tahooey/wrld/WorldRunner.java
@@ -10,6 +10,14 @@ public class WorldRunner {
 	public static void RunWorld(Graphics g){
 		setBlocksXY();
 		drawLowerTiles(g);
+		runEntities(g);
+	}
+	
+	public static void runEntities(Graphics g){
+		for(int i=0;i<Engine.mb.ENTITIES.length;i++){
+			Engine.mb.ENTITIES[i].runEntity(g);
+		}
+		
 	}
 	public static void setBlocksXY(){
 		for(int i=0;i<Engine.mb.LowerBLOCKS.length;i++){
@@ -17,9 +25,11 @@ public class WorldRunner {
 				Engine.mb.LowerBLOCKS[i][j].x=i*Block.WIDTH;
 				Engine.mb.LowerBLOCKS[i][j].y=j*Block.HEIGHT;
 				Engine.mb.LowerBLOCKS[i][j].isHigh=false;
+				Engine.mb.LowerBLOCKS[i][j].layer=1;
 				Engine.mb.HigherBLOCKS[i][j].x=i*Block.WIDTH;
 				Engine.mb.HigherBLOCKS[i][j].y=j*Block.HEIGHT;
 				Engine.mb.HigherBLOCKS[i][j].isHigh=true;
+				Engine.mb.HigherBLOCKS[i][j].layer=2;
 			}
 		}
 	}
