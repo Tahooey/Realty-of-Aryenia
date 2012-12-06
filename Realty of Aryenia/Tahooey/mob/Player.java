277ffb1c9088acb9c91192b00234bffe025d0bef
Will <theonlytahooey@hotmail.com>
2012-12-06 19:59:59 +1100

    New Player

--- Realty of Aryenia/Tahooey/mob/Player.java
+++ Realty of Aryenia/Tahooey/mob/Player.java
@@ -1,9 +1,16 @@
 package mob;
 
+import img.ImageHandler;
+
 public class Player extends Entity{
 	
 	public Player(){
+		isFollowedByCamera=true;
 		canCollide=true;
+		x=8;
+		y=8;
+		IMG_TO_DRAW=ImageHandler.Player;
+		currentLayer=2;
 	}
 
 }
