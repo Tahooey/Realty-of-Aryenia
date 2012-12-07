dd5cee5e5385a4aaade5ae1e78838787afd70343
Will <theonlytahooey@hotmail.com>
2012-12-07 22:51:25 +1100

    Follow's Entity that is chosen to be followed

--- Realty of Aryenia/Tahooey/def/Camera.java
+++ Realty of Aryenia/Tahooey/def/Camera.java
@@ -3,29 +3,19 @@ package def;
 public class Camera {
 	
 	public int xOffset,yOffset;
+	public int dx,dy;
 	public static int UP=1,DOWN=2,LEFT=3,RIGHT=4;
 	
-	public Camera(int x,int y){
-		setXY(x,y);
+	public Camera(){
+		
 	}
 	
-	public void setXY(int x, int y){
-		xOffset=x;
-		yOffset=y;
-	}
-	
-	public void move(int dir){
-		if(dir==UP){
-			yOffset+=Frame.SPEED;
-		}
-		if(dir==DOWN){
-			yOffset-=Frame.SPEED;
-		}
-		if(dir==LEFT){
-			xOffset+=Frame.SPEED;
-		}
-		if(dir==RIGHT){
-			xOffset-=Frame.SPEED;
+	public void move(){
+		for(int i=0;i<Engine.mb.ENTITIES.length;i++){
+			if(Engine.mb.ENTITIES[i].isFollowedByCamera){
+				xOffset=(Engine.mb.ENTITIES[i].xoffset*-1)+400;
+				yOffset=(Engine.mb.ENTITIES[i].yoffset*-1)+200;				
+			}
 		}
 	}
 
