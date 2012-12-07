ae5f3bc0ab5d09f840755c75f2d68704c6a8e42d
Will <theonlytahooey@hotmail.com>
2012-12-07 22:51:39 +1100

    Engine Change 7/12/12

--- Realty of Aryenia/Tahooey/def/Engine.java
+++ Realty of Aryenia/Tahooey/def/Engine.java
@@ -2,33 +2,70 @@ package def;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 
 import mob.Entity;
 
+import til.Block;
 import wrld.*;
 
 public class Engine {
 	
 	public static WorldFileHandler fh = new WorldFileHandler();
 	public static MapBuilder mb = new MapBuilder();
-	public static Camera cam = new Camera(400,200);
+	public static Camera cam = new Camera();
 	public static void RunGame(Graphics g){
 		WorldRunner.RunWorld(g);
+		cam.move();
 	}
 	public static void KeyPressed(KeyEvent e){
 		int KeyCode = e.getKeyCode();
-		if(KeyCode==KeyEvent.VK_W){
-			mb.ENTITIES[0].move(Entity.UP);
+		for(int i=0;i<mb.ENTITIES.length;i++){
+			if(mb.ENTITIES[i].isControlledByPlayer){
+				if(KeyCode==KeyEvent.VK_W){
+					mb.ENTITIES[i].move(Entity.UP);
+				}
+				if(KeyCode==KeyEvent.VK_S){
+					mb.ENTITIES[i].move(Entity.DOWN);
+				}
+				if(KeyCode==KeyEvent.VK_A){
+					mb.ENTITIES[i].move(Entity.LEFT);
+				}
+				if(KeyCode==KeyEvent.VK_D){
+					mb.ENTITIES[i].move(Entity.RIGHT);
+				}				
+			}
 		}
-		if(KeyCode==KeyEvent.VK_S){
-			mb.ENTITIES[0].move(Entity.DOWN);
+		
+		if(KeyCode==KeyEvent.VK_P){
+			Entity.canStartAI=true;
 		}
-		if(KeyCode==KeyEvent.VK_A){
-			mb.ENTITIES[0].move(Entity.LEFT);
+
+	}
+	public static void KeyReleased(KeyEvent e){
+		int KeyCode = e.getKeyCode();
+		
+		for(int i=0;i<mb.ENTITIES.length;i++){
+			if(mb.ENTITIES[i].isControlledByPlayer){
+				if(KeyCode==KeyEvent.VK_W){
+					mb.ENTITIES[i].dy=0;
+				}
+				if(KeyCode==KeyEvent.VK_S){
+					mb.ENTITIES[i].dy=0;
+				}
+				if(KeyCode==KeyEvent.VK_A){
+					mb.ENTITIES[i].dx=0;
+				}
+				if(KeyCode==KeyEvent.VK_D){
+					mb.ENTITIES[i].dx=0;
+				}
+				mb.ENTITIES[i].canLetMove=true;				
+			}
 		}
-		if(KeyCode==KeyEvent.VK_D){
-			mb.ENTITIES[0].move(Entity.RIGHT);
-		}
+		
+
+	}
+	public static void MousePressed(MouseEvent e){
 	}
 
 }
