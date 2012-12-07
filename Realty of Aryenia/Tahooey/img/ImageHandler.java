675462cbe407f4e7f7fb05c4227ab54514dc861e
Will <theonlytahooey@hotmail.com>
2012-12-07 22:52:36 +1100

    Wizard Image and Flame Image

--- Realty of Aryenia/Tahooey/img/ImageHandler.java
+++ Realty of Aryenia/Tahooey/img/ImageHandler.java
@@ -13,11 +13,15 @@ public class ImageHandler {
 	public static BufferedImage terrainHigher;
 	public static BufferedImage terrainLower;
 	public static BufferedImage Player;
+	public static BufferedImage PlayerFire;
+	public static BufferedImage Wizard;
 	
 	public static void load() throws IOException{
 		terrainHigher=loadImage("terrainHigher.png");
 		terrainLower=loadImage("terrainLower.png");
 		Player=loadImage("mob0.png");
+		PlayerFire=loadImage("mob0fire.png");
+		Wizard=loadImage("mob1.png");
 	}
 	
 	public static BufferedImage loadImage(String path) throws IOException {
