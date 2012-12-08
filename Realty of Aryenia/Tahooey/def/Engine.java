d4a69f63e66f5c24fd2b31766933a70e5f2d1738
Will <theonlytahooey@hotmail.com>
2012-12-08 22:14:04 +1100

    Engine Change 8/12/12

--- Realty of Aryenia/Tahooey/def/Engine.java
+++ Realty of Aryenia/Tahooey/def/Engine.java
@@ -4,14 +4,14 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
-import mob.Entity;
-
-import til.Block;
+import mob.*;
 import wrld.*;
 
 public class Engine {
 	
-	public static WorldFileHandler fh = new WorldFileHandler();
+	public static boolean canPressKey=true;
+	public static MobFileHandler mfh = new MobFileHandler();
+	public static WorldFileHandler fh = new WorldFileHandler();	
 	public static MapBuilder mb = new MapBuilder();
 	public static Camera cam = new Camera();
 	public static void RunGame(Graphics g){
@@ -22,25 +22,32 @@ public class Engine {
 		int KeyCode = e.getKeyCode();
 		for(int i=0;i<mb.ENTITIES.length;i++){
 			if(mb.ENTITIES[i].isControlledByPlayer){
-				if(KeyCode==KeyEvent.VK_W){
-					mb.ENTITIES[i].move(Entity.UP);
+				if(canPressKey){
+					if(KeyCode==KeyEvent.VK_W){
+						mb.ENTITIES[i].move(Entity.UP);
+						canPressKey=false;
+					}
+					if(KeyCode==KeyEvent.VK_S){
+						mb.ENTITIES[i].move(Entity.DOWN);
+						canPressKey=false;
+					}
+					if(KeyCode==KeyEvent.VK_A){
+						mb.ENTITIES[i].move(Entity.LEFT);
+						canPressKey=false;
+					}
+					if(KeyCode==KeyEvent.VK_D){
+						mb.ENTITIES[i].move(Entity.RIGHT);
+						canPressKey=false;
+					}						
 				}
-				if(KeyCode==KeyEvent.VK_S){
-					mb.ENTITIES[i].move(Entity.DOWN);
-				}
-				if(KeyCode==KeyEvent.VK_A){
-					mb.ENTITIES[i].move(Entity.LEFT);
-				}
-				if(KeyCode==KeyEvent.VK_D){
-					mb.ENTITIES[i].move(Entity.RIGHT);
-				}				
+			
 			}
 		}
 
 	}
 	public static void KeyReleased(KeyEvent e){
 		int KeyCode = e.getKeyCode();
-		
+		canPressKey=true;
 		for(int i=0;i<mb.ENTITIES.length;i++){
 			if(mb.ENTITIES[i].isControlledByPlayer){
 				mb.ENTITIES[i].changingX=false;
