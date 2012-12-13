5563d9567616e1398cbc5d5814e3d6e28ab30b4e
Will <theonlytahooey@hotmail.com>
2012-12-13 23:08:24 +1100

    Much better Hit Boxes

--- Realty of Aryenia v2/Tahooey/blcks/Block.java
+++ Realty of Aryenia v2/Tahooey/blcks/Block.java
@@ -7,6 +7,7 @@ import fil.ImageFileHandler;
 
 public class Block {
 	
+	public boolean isIntersectedByMouse=false;
 	public int ID;
 	public Image IMG_TO_DRAW;
 	public int imgx,imgy,imgx2,imgy2;
@@ -16,7 +17,7 @@ public class Block {
 	public int finalx,finaly,finalw,finalh;
 	public boolean isCollidable=true;
 	
-	Rectangle r = new Rectangle();
+	public Rectangle r = new Rectangle();
 	public Block(){
 		
 	}
@@ -48,16 +49,24 @@ public class Block {
 			for(int i=0;i<Engine.mb.MOBS.length;i++){
 				if(Engine.mb.MOBS[i].Layer==l){
 					if(Engine.mb.MOBS[i].up.intersects(r)){
-						Engine.mb.MOBS[i].canMoveUp=false;
+						if(!Engine.mb.MOBS[i].GoingDown){
+							Engine.mb.MOBS[i].dy=0;	
+						}													
 					}
 					if(Engine.mb.MOBS[i].down.intersects(r)){
-						Engine.mb.MOBS[i].canMoveDown=false;
+						if(!Engine.mb.MOBS[i].GoingUp){
+							Engine.mb.MOBS[i].dy=0;
+						}						
 					}
 					if(Engine.mb.MOBS[i].left.intersects(r)){
-						Engine.mb.MOBS[i].canMoveLeft=false;
+						if(!Engine.mb.MOBS[i].GoingRight){
+							Engine.mb.MOBS[i].dx=0;
+						}						
 					}
 					if(Engine.mb.MOBS[i].right.intersects(r)){
-						Engine.mb.MOBS[i].canMoveRight=false;
+						if(!Engine.mb.MOBS[i].GoingLeft){
+							Engine.mb.MOBS[i].dx=0;
+						}						
 					}
 				}
 			}
@@ -73,10 +82,15 @@ public class Block {
 						if(l==Engine.mb.MOBS[0].Layer){
 							IMG_TO_DRAW=ImageFileHandler.TerrainHigher;
 							g.drawImage(IMG_TO_DRAW,finalx+Engine.cam.x,finaly+Engine.cam.y,finalx+finalw+Engine.cam.x,finaly+finalh+Engine.cam.y,imgx,imgy,imgx2,imgy2,null);
-						}else if(l==Engine.mb.MOBS[0].Layer-1){
+							
+						}
+						if(l==Engine.mb.MOBS[0].Layer-1){
+							isIntersectedByMouse=false;
+						}
+						if(l==Engine.mb.MOBS[0].Layer-1){
 							IMG_TO_DRAW=ImageFileHandler.TerrainLower;
 							g.drawImage(IMG_TO_DRAW,finalx+Engine.cam.x,finaly+Engine.cam.y,finalx+finalw+Engine.cam.x,finaly+finalh+Engine.cam.y,imgx,imgy,imgx2,imgy2,null);
-						}						
+						}				
 					}
 				}
 			}
