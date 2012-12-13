64413353393243135ad49386207bc7be3e51c3b2
Will <theonlytahooey@hotmail.com>
2012-12-13 23:09:02 +1100

    Engine Update 13/12/12

--- Realty of Aryenia v2/Tahooey/def/Engine.java
+++ Realty of Aryenia v2/Tahooey/def/Engine.java
@@ -2,6 +2,7 @@ package def;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
 
 import world.*;
 import fil.*;
@@ -17,31 +18,71 @@ public class Engine {
 	public static void RunGame(Graphics g){
 		wr.RunWorld(g);
 		cam.RunCam();
+		Mouse.Update();
+	}
+	
+	public static void Initialise(){
+		Mouse.Init();
+	}
+	
+	public static void MousePressed(MouseEvent e){
+		Mouse.isPressed=true;
+	}
+	
+	public static void MouseReleased(MouseEvent e){
+		Mouse.isPressed=false;
+	}
+	
+	public static void MouseMoved(MouseEvent e){
+		int x=e.getX();
+		int y=e.getY();
+		Mouse.setXY(x, y);
 	}
 	
 	public static void KeyPressed(KeyEvent e){
 		int KeyCode=e.getKeyCode();
 		if(KeyCode==KeyEvent.VK_W){
-			cam.move(Frame.UP);
 			mb.MOBS[0].move(Frame.UP);
 		}
 		if(KeyCode==KeyEvent.VK_S){
-			cam.move(Frame.DOWN);
 			mb.MOBS[0].move(Frame.DOWN);
 		}
 		if(KeyCode==KeyEvent.VK_A){
-			cam.move(Frame.LEFT);
 			mb.MOBS[0].move(Frame.LEFT);
 		}
 		if(KeyCode==KeyEvent.VK_D){
-			cam.move(Frame.RIGHT);
 			mb.MOBS[0].move(Frame.RIGHT);
 		}
 		if(KeyCode==KeyEvent.VK_UP){
-			mb.MOBS[0].Layer+=2;
+			if(mb.MOBS[0].Layer<Engine.mb.BLOCKS.length-1){
+				mb.MOBS[0].Layer+=1;
+			}else{
+				mb.MOBS[0].Layer=mb.BLOCKS.length-2;
+			}
 		}
 		if(KeyCode==KeyEvent.VK_DOWN){
-			mb.MOBS[0].Layer-=2;
+			if(mb.MOBS[0].Layer>0+1){
+				mb.MOBS[0].Layer-=1;
+			}else{
+				mb.MOBS[0].Layer=0+1;
+			}
+		}
+		if(KeyCode==KeyEvent.VK_RIGHT){
+			if(Mouse.blockToChangeTo<Mouse.maxBlock){
+				Mouse.blockToChangeTo+=1;
+			}			
+		}
+		if(KeyCode==KeyEvent.VK_LEFT){
+			if(Mouse.blockToChangeTo>0){
+			Mouse.blockToChangeTo-=1;
+			}
+		}
+		if(KeyCode==KeyEvent.VK_P){
+			try {
+				wfh.SaveWorld();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
 		}
 	}
 	public static void KeyReleased(KeyEvent e){
