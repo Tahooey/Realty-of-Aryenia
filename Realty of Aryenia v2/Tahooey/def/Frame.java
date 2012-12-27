60d9e746a446b8d40e431fccb0818050a20934a6
Will <theonlytahooey@hotmail.com>
2012-12-27 15:06:44 +1100

    Implementes Game Engine
    From TheJavaHub

--- Realty of Aryenia v2/Tahooey/def/Frame.java
+++ Realty of Aryenia v2/Tahooey/def/Frame.java
@@ -1,7 +1,6 @@
 package def;
 
 import java.awt.*;
-import java.awt.event.*;
 import java.io.*;
 
 import javax.swing.*;
@@ -10,26 +9,33 @@ import fil.FontFileHandler;
 import fil.ImageFileHandler;
 import fil.SoundFileHandler;
 
-public class Frame extends JFrame implements KeyListener, MouseListener, MouseMotionListener {
+public class Frame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	Image dbImage;
 	Graphics dbg;
+	public final static Color BACKGROUND = Color.BLACK;
 	public static int SPEED;
-	public static final int UP=1,DOWN=2,LEFT=3,RIGHT=4,STILL=5;
-	public static  final int SCALE=8;
-	public static final int WIDTH=1028,HEIGHT=576;
-	public static final String TITLE="Realty of Aryenia";
-	
-	public Frame(){
-		SPEED=1*SCALE;
-		setSize(WIDTH,HEIGHT);
+	public static final int UP = 1, DOWN = 2, LEFT = 3, RIGHT = 4, STILL = 5;
+	public static final int SCALE = 8;
+	public static final int WIDTH = 1028, HEIGHT = 576;
+	public static final String TITLE = "Realty of Aryenia";
+	GamePanel gp;
+
+	public Frame() {
+
+		gp = new GamePanel();
+
+		SPEED = 1 * SCALE;
+		setSize(WIDTH, HEIGHT);
 		setLocationRelativeTo(null);
 		setTitle(TITLE);
-		setBackground(Color.black);
+		setBackground(BACKGROUND);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
-		
-		//IO
+
+		add(gp);
+
+		// IO
 		try {
 			ImageFileHandler.loadImages();
 			SoundFileHandler.load();
@@ -39,101 +45,10 @@ public class Frame extends JFrame implements KeyListener, MouseListener, MouseMo
 		} catch (FontFormatException e) {
 			e.printStackTrace();
 		}
-		
-		//Listeners
-		addKeyListener(this);
-		addMouseListener(this);
-		addMouseMotionListener(this);
-		
-		//Initialise
+
 		Engine.Initialise();
-		SoundFileHandler.sound1.loop();
-		
+
 		setVisible(true);
-	}
-	
-	Thread BlockRunner = new Thread(){
-		public void run(){
-			Engine.wr.RunBlocks();
-		}
-	};
-	Thread MobRunner = new Thread(){
-		public void run(){
-			Engine.wr.RunMobs();
-			Engine.cam.RunCam();
-		}
-	};
-	Thread MouseUpdater = new Thread(){
-		public void run(){
-			Mouse.Update();
-		}
-	};
-	
-	public void paintComponent(Graphics g){
-		intrfce.Menu.runMenu(g);
-		Engine.wr.RunWorld(g);
-		MouseUpdater.run();
-		BlockRunner.run();
-		MobRunner.run();
-		repaint();
-	}
-	
-	public void paint(Graphics g){
-		dbImage=createImage(getWidth(), getHeight());
-		dbg = dbImage.getGraphics();		
-		paintComponent(dbg);
-		g.drawImage(dbImage, 0, 0, this);
-	}
-
-	@Override
-	public void keyPressed(KeyEvent arg0) {
-		Engine.KeyPressed(arg0);
-	}
-
-	@Override
-	public void keyReleased(KeyEvent arg0) {
-		Engine.KeyReleased(arg0);
-	}
-
-	@Override
-	public void keyTyped(KeyEvent arg0) {
-		
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent arg0) {
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		Engine.MousePressed(arg0);
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		Engine.MouseReleased(arg0);
-	}
-
-	@Override
-	public void mouseDragged(MouseEvent arg0) {
-		Engine.MouseMoved(arg0);
-		
-	}
-
-	@Override
-	public void mouseMoved(MouseEvent arg0) {
-		Engine.MouseMoved(arg0);
 	}
 
 }
