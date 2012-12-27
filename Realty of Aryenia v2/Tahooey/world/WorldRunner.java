c39df14e484c47f86dc00848177c52a55eab76ff
Will <theonlytahooey@hotmail.com>
2012-12-27 15:08:18 +1100

    Formatted

--- Realty of Aryenia v2/Tahooey/world/WorldRunner.java
+++ Realty of Aryenia v2/Tahooey/world/WorldRunner.java
@@ -5,42 +5,47 @@ import java.awt.*;
 import def.Engine;
 
 public class WorldRunner {
-	
-	public WorldRunner(){
-		
+
+	public WorldRunner() {
+
 	}
-	
-	public void RunWorld(Graphics g){
+
+	public void RunWorld(Graphics g) {
 		RunBlocksImg(g);
 		RunMobsImg(g);
 	}
-	
-	public void RunMobsImg(Graphics g){
-		for(int i=0;i<Engine.mb.MOBS.length;i++){
+
+	public void RunMobsImg(Graphics g) {
+		for (int i = 0; i < Engine.mb.MOBS.length; i++) {
 			Engine.mb.MOBS[i].drawMob(g);
 		}
 	}
-	
-	public void RunBlocksImg(Graphics g){
-		for(int l=0;l<Engine.mb.BLOCKS.length;l++){
-			for(int h=0;h<Engine.mb.BLOCKS[l].length;h++){
-				for(int w=0;w<Engine.mb.BLOCKS[l][h].length;w++){
+
+	public void RunBlocksImg(Graphics g) {
+		for (int l = 0; l < Engine.mb.BLOCKS.length; l++) {
+			for (int h = 0; h < Engine.mb.BLOCKS[l].length; h++) {
+				for (int w = 0; w < Engine.mb.BLOCKS[l][h].length; w++) {
 					Engine.mb.BLOCKS[l][h][w].drawBlock(g);
 				}
 			}
 		}
 	}
-	
-	public void RunMobs(){
-		for(int i=0;i<Engine.mb.MOBS.length;i++){
+
+	public void Update() {
+		RunMobs();
+		RunBlocks();
+	}
+
+	public void RunMobs() {
+		for (int i = 0; i < Engine.mb.MOBS.length; i++) {
 			Engine.mb.MOBS[i].runMob();
 		}
 	}
-	
-	public void RunBlocks(){
-		for(int l=0;l<Engine.mb.BLOCKS.length;l++){
-			for(int h=0;h<Engine.mb.BLOCKS[l].length;h++){
-				for(int w=0;w<Engine.mb.BLOCKS[l][h].length;w++){
+
+	public void RunBlocks() {
+		for (int l = 0; l < Engine.mb.BLOCKS.length; l++) {
+			for (int h = 0; h < Engine.mb.BLOCKS[l].length; h++) {
+				for (int w = 0; w < Engine.mb.BLOCKS[l][h].length; w++) {
 					Engine.mb.BLOCKS[l][h][w].RunBlock();
 				}
 			}
