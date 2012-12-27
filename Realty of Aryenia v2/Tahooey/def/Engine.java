4c4d21faa623846f9207a6a8aaa595ffaa0a5bff
Will <theonlytahooey@hotmail.com>
2012-12-27 15:06:07 +1100

    Better Handling of Menu + Threads

--- Realty of Aryenia v2/Tahooey/def/Engine.java
+++ Realty of Aryenia v2/Tahooey/def/Engine.java
@@ -8,112 +8,148 @@ import world.*;
 import fil.*;
 
 public class Engine {
-	
-	public static boolean gameIsRunning=false;
-	
-	public static WorldFileHandler wfh = new WorldFileHandler("test");
-	public static PlayerFileHandler pfh = new PlayerFileHandler("test");
+
+	public static boolean gameIsRunning = false;
+
+	public static WorldFileHandler wfh = new WorldFileHandler("MaybeMap");
+	public static PlayerFileHandler pfh = new PlayerFileHandler("MaybeMap");
 	public static MapBuilder mb = new MapBuilder();
 	public static WorldRunner wr = new WorldRunner();
 	public static Camera cam = new Camera();
-	
-	public static void RunGame(Graphics g){
-		//if(!gameIsRunning){
-			//intrfce.Menu.runMenu(g);
-		//}else{
-			wr.RunWorld(g);
-		//}
+
+	public static void RunGame(Graphics g) {
+		wr.RunWorld(g);
+		if (intrfce.Menu.runMenu) {
+			intrfce.Menu.runMenu(g);
+		}
 	}
-	public static void Initialise(){
+
+	public static void GameUpdate() {
+		BlockUpdater.run();
+		MobUpdater.run();
+		CamUpdater.run();
+		MouseUpdater.run();
+		MenuDecider.run();
+	}
+	
+	static Thread BlockUpdater = new Thread(){
+		public void run(){
+			wr.RunBlocks();
+		}
+	};
+	static Thread MobUpdater = new Thread(){
+		public void run(){
+			wr.RunMobs();
+		}
+	};
+	static Thread CamUpdater = new Thread(){
+		public void run(){
+			cam.RunCam();
+		}
+	};
+	static Thread MouseUpdater = new Thread(){
+		public void run(){
+			Mouse.Update();
+		}
+	};
+	static Thread MenuDecider = new Thread(){
+		public void run(){
+			if(intrfce.Menu.runMenu){
+				intrfce.Menu.menuUpdate();
+			}
+		}
+	};
+
+	public static void Initialise() {
 		Mouse.Init();
 		intrfce.Menu.Init();
 	}
-	
-	public static void MousePressed(MouseEvent e){
-		Mouse.isPressed=true;
+
+	public static void MousePressed(MouseEvent e) {
+		Mouse.isPressed = true;
 	}
-	
-	public static void MouseReleased(MouseEvent e){
-		Mouse.isPressed=false;
+
+	public static void MouseReleased(MouseEvent e) {
+		Mouse.isPressed = false;
 	}
-	
-	public static void MouseMoved(MouseEvent e){
-		int x=e.getX();
-		int y=e.getY();
+
+	public static void MouseMoved(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
 		Mouse.setXY(x, y);
 	}
-	
-	public static void KeyPressed(KeyEvent e){
-		int KeyCode=e.getKeyCode();
-		//if(intrfce.Menu.runMenu==false){
-			if(KeyCode==KeyEvent.VK_W){
+
+	public static void KeyPressed(KeyEvent e) {
+		int KeyCode = e.getKeyCode();
+		if (intrfce.Menu.runMenu == false) {
+			if (KeyCode == KeyEvent.VK_W) {
 				mb.MOBS[0].move(Frame.UP);
 			}
-			if(KeyCode==KeyEvent.VK_S){
+			if (KeyCode == KeyEvent.VK_S) {
 				mb.MOBS[0].move(Frame.DOWN);
 			}
-			if(KeyCode==KeyEvent.VK_A){
+			if (KeyCode == KeyEvent.VK_A) {
 				mb.MOBS[0].move(Frame.LEFT);
 			}
-			if(KeyCode==KeyEvent.VK_D){
+			if (KeyCode == KeyEvent.VK_D) {
 				mb.MOBS[0].move(Frame.RIGHT);
 			}
-		//}
-		if(KeyCode==KeyEvent.VK_O){
+		}
+		if (KeyCode == KeyEvent.VK_O) {
 			SoundFileHandler.sound1.stop();
 		}
-		if(KeyCode==KeyEvent.VK_ESCAPE){
-			//intrfce.Menu.runMenu=true;
+		if (KeyCode == KeyEvent.VK_ESCAPE) {
+			intrfce.Menu.runMenu = true;
 		}
-		/**if(KeyCode==KeyEvent.VK_UP){
-			if(mb.MOBS[0].Layer<Engine.mb.BLOCKS.length-1){
-				mb.MOBS[0].Layer+=1;
-			}else{
-				mb.MOBS[0].Layer=mb.BLOCKS.length-2;
+		if (KeyCode == KeyEvent.VK_UP) {
+			if (mb.MOBS[0].Layer < Engine.mb.BLOCKS.length - 1) {
+				mb.MOBS[0].Layer += 1;
+			} else {
+				mb.MOBS[0].Layer = mb.BLOCKS.length - 2;
 			}
 		}
-		if(KeyCode==KeyEvent.VK_DOWN){
-			if(mb.MOBS[0].Layer>0+1){
-				mb.MOBS[0].Layer-=1;
-			}else{
-				mb.MOBS[0].Layer=0+1;
+		if (KeyCode == KeyEvent.VK_DOWN) {
+			if (mb.MOBS[0].Layer > 0 + 1) {
+				mb.MOBS[0].Layer -= 1;
+			} else {
+				mb.MOBS[0].Layer = 0 + 1;
 			}
-		}**/
-		if(KeyCode==KeyEvent.VK_RIGHT){
-			if(Mouse.blockToChangeTo<Mouse.maxBlock){
-				Mouse.blockToChangeTo+=1;
-			}			
 		}
-		if(KeyCode==KeyEvent.VK_LEFT){
-			if(Mouse.blockToChangeTo>0){
-			Mouse.blockToChangeTo-=1;
+		if (KeyCode == KeyEvent.VK_RIGHT) {
+			if (Mouse.blockToChangeTo < Mouse.maxBlock) {
+				Mouse.blockToChangeTo += 1;
+			}
+		}
+		if (KeyCode == KeyEvent.VK_LEFT) {
+			if (Mouse.blockToChangeTo > 0) {
+				Mouse.blockToChangeTo -= 1;
 			}
 		}
 	}
-	
-	public static void save(){
+
+	public static void save() {
 		try {
 			wfh.SaveWorld();
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
 	}
-	
-	public static void KeyReleased(KeyEvent e){
-		int KeyCode=e.getKeyCode();
-		if(KeyCode==KeyEvent.VK_W){
+
+	public static void KeyReleased(KeyEvent e) {
+		int KeyCode = e.getKeyCode();
+		if (KeyCode == KeyEvent.VK_W) {
 			cam.move(Frame.STILL);
 			mb.MOBS[0].move(Frame.STILL);
 		}
-		if(KeyCode==KeyEvent.VK_S){
+		if (KeyCode == KeyEvent.VK_S) {
 			cam.move(Frame.STILL);
 			mb.MOBS[0].move(Frame.STILL);
 		}
-		if(KeyCode==KeyEvent.VK_A){
+		if (KeyCode == KeyEvent.VK_A) {
 			cam.move(Frame.STILL);
 			mb.MOBS[0].move(Frame.STILL);
 		}
-		if(KeyCode==KeyEvent.VK_D){
+		if (KeyCode == KeyEvent.VK_D) {
 			cam.move(Frame.STILL);
 			mb.MOBS[0].move(Frame.STILL);
 		}
