6d0dcf7d33df1bf1a0744f0e7e4c6cd5c2e296b5
Will <theonlytahooey@hotmail.com>
2013-01-06 13:50:00 -0800

    Camera Changing

--- Realty of Aryenia v2/Tahooey/def/Camera.java
+++ Realty of Aryenia v2/Tahooey/def/Camera.java
@@ -1,41 +1,46 @@
 package def;
 
 public class Camera {
-	
-	public int x=0,y=0;
-	public int dx=0,dy=0;
-	
-	public Camera(){
-		
+
+	public int x = 0, y = 0;
+	public int dx = 0, dy = 0;
+
+	public Camera() {
+
 	}
-	
-	public void RunCam(){
-		x=(Engine.mb.MOBS[0].finalx*-1)+500;
-		y=(Engine.mb.MOBS[0].finaly*-1)+200;
-		x=x+dx;
-		y=y+dy;
+
+	public void RunCam() {
+		if (!Engine.editMode) {
+			x = (Engine.mb.MOBS[0].finalx * -1) + 500;
+			y = (Engine.mb.MOBS[0].finaly * -1) + 200;
+			x = x + dx;
+			y = y + dy;
+		}else{
+			x=x+dx;
+			y=y+dy;
+		}
 	}
-	
-	public void move(int dir){
-		if(dir==def.Frame.LEFT){
-			dx=Frame.SPEED;
-			dy=0;
+
+	public void move(int dir) {
+		if (dir == def.Frame.LEFT) {
+			dx = Frame.SPEED;
+			dy = 0;
 		}
-		if(dir==def.Frame.RIGHT){
-			dx=-Frame.SPEED;
-			dy=0;
+		if (dir == def.Frame.RIGHT) {
+			dx = -Frame.SPEED;
+			dy = 0;
 		}
-		if(dir==def.Frame.UP){
-			dy=Frame.SPEED;
-			dx=0;
+		if (dir == def.Frame.UP) {
+			dy = Frame.SPEED;
+			dx = 0;
 		}
-		if(dir==def.Frame.DOWN){
-			dy=-Frame.SPEED;
-			dx=0;
+		if (dir == def.Frame.DOWN) {
+			dy = -Frame.SPEED;
+			dx = 0;
 		}
-		if(dir==def.Frame.STILL){
-			dy=0;
-			dx=0;
+		if (dir == def.Frame.STILL) {
+			dy = 0;
+			dx = 0;
 		}
 	}
 
