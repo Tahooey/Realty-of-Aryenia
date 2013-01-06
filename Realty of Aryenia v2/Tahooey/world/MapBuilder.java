09cfc7ebaa78b9dd3bd88ebab59631d094e2849d
Will <theonlytahooey@hotmail.com>
2013-01-06 13:54:14 -0800

    Changed how maps are loaded/run

--- Realty of Aryenia v2/Tahooey/world/MapBuilder.java
+++ Realty of Aryenia v2/Tahooey/world/MapBuilder.java
@@ -1,5 +1,9 @@
 package world;
 
+import itm.*;
+
+import java.io.IOException;
+
 import mob.*;
 import def.*;
 import blcks.*;
@@ -9,32 +13,72 @@ public class MapBuilder {
 	public Block[][][] BLOCKS;
 	public Mob[] MOBS;
 	public int[][][] ID;
+	public int currentmap;
 
-	public MapBuilder() {
+	int currentab = 0;
+
+	public MapBuilder(int map) {
+		map=Engine.mfo.pfh.ReadPlayerMap;
+		currentmap = Engine.mfo.pfh.ReadPlayerMap;
 		MOBS = new Mob[1];
-		BLOCKS = new Block[Engine.wfh.ReadWorldLayers][Engine.wfh.ReadWorldH][Engine.wfh.ReadWorldW];
-		ID = Engine.wfh.ReadBlockID;
+		BLOCKS = new Block[Engine.mfo.WFH[map].ReadWorldLayers][Engine.mfo.WFH[map].ReadWorldH][Engine.mfo.WFH[map].ReadWorldW];
+		ID = Engine.mfo.WFH[map].ReadBlockID;
 		BuildMap();
+	}
+
+	public void ChangeMap(int toChange) {
+		Engine.changingMaps = true;
+		MOBS[0].map=toChange;
+		currentab = 0;
+		def.Frame.gp.blankScreen = true;
+		currentmap = toChange;
+		BLOCKS = new Block[Engine.mfo.WFH[currentmap].ReadWorldLayers][Engine.mfo.WFH[currentmap].ReadWorldH][Engine.mfo.WFH[currentmap].ReadWorldW];
+		ID = Engine.mfo.WFH[currentmap].ReadBlockID;
+		BuildBlocks();
+		def.Frame.gp.blankScreen = false;
+		Engine.changingMaps = false;
+	}
+
+	public void save() {
+		try {
+			Engine.mfo.WFH[currentmap].SaveWorld();
+			Engine.mfo.pfh.save();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void BuildMap() {
 		BuildBlocks();
 		BuildMobs();
+		currentab = 0;
 	}
 
 	public void BuildMobs() {
 		for (int i = 0; i < MOBS.length; i++) {
-			if (Engine.pfh.ReadPlayerType == 1) {
-				MOBS[i] = new Sleeper(Engine.pfh.ReadPlayerX,
-						Engine.pfh.ReadPlayerY, Engine.pfh.ReadPlayerName,
-						Engine.pfh.ReadPlayerLayer);
-			} else if (Engine.pfh.ReadPlayerType == 2) {
-				MOBS[i] = new Wizard(Engine.pfh.ReadPlayerX,
-						Engine.pfh.ReadPlayerY, Engine.pfh.ReadPlayerName,
-						Engine.pfh.ReadPlayerLayer);
+			if (Engine.mfo.pfh.ReadPlayerType == 1) {
+				MOBS[i] = new Sleeper(Engine.mfo.pfh.ReadPlayerX,
+						Engine.mfo.pfh.ReadPlayerY,
+						Engine.mfo.pfh.ReadPlayerName,
+						Engine.mfo.pfh.ReadPlayerLayer);
+			} else if (Engine.mfo.pfh.ReadPlayerType == 2) {
+				MOBS[i] = new Wizard(Engine.mfo.pfh.ReadPlayerX,
+						Engine.mfo.pfh.ReadPlayerY,
+						Engine.mfo.pfh.ReadPlayerName,
+						Engine.mfo.pfh.ReadPlayerLayer);
 			}
 			if (i == 0) {
 				MOBS[i].isControlledByPlayer = true;
+				MOBS[i].map=Engine.mfo.pfh.ReadPlayerMap;
+				for(int h=0;h<MOBS[i].ITEMS.length;h++){
+					for(int w=0;w<MOBS[i].ITEMS[h].length;w++){
+						if(Engine.mfo.pfh.ITEMS[h][w]==0){
+							MOBS[i].ITEMS[h][w]=new NoItem();
+						}else if(Engine.mfo.pfh.ITEMS[h][w]==1){
+							MOBS[i].ITEMS[h][w]=new Candle();
+						} 
+					}
+				}
 			}
 		}
 	}
@@ -68,6 +112,55 @@ public class MapBuilder {
 						BLOCKS[l][h][w] = new Wood();
 					} else if (ID[l][h][w] == 10) {
 						BLOCKS[l][h][w] = new Stone();
+					} else if (ID[l][h][w] == 11) {
+						if (currentab < Engine.mfo.WFH[currentmap].ReadActionBlocksMapToChangeTo.length) {
+							BLOCKS[l][h][w] = new MapChanger(
+									Engine.mfo.WFH[currentmap].ReadActionBlocksMapToChangeTo[currentab],
+									Engine.mfo.WFH[currentmap].ReadActionBlocksPlayerX[currentab],
+									Engine.mfo.WFH[currentmap].ReadActionBlocksPlayerY[currentab],
+									Block.UP);
+							currentab += 1;
+						} else {
+							BLOCKS[l][h][w] = new MapChanger(currentmap,0,0,
+									Block.UP);
+						}
+					} else if (ID[l][h][w] == 12) {
+						if (currentab < Engine.mfo.WFH[currentmap].ReadActionBlocksMapToChangeTo.length) {
+							BLOCKS[l][h][w] = new MapChanger(
+									Engine.mfo.WFH[currentmap].ReadActionBlocksMapToChangeTo[currentab],
+									Engine.mfo.WFH[currentmap].ReadActionBlocksPlayerX[currentab],
+									Engine.mfo.WFH[currentmap].ReadActionBlocksPlayerY[currentab],
+									Block.DOWN);
+							currentab += 1;
+						} else {
+							BLOCKS[l][h][w] = new MapChanger(currentmap,0,0,
+									Block.DOWN);
+						}
+						currentab += 1;
+					} else if (ID[l][h][w] == 13) {
+						if (currentab < Engine.mfo.WFH[currentmap].ReadActionBlocksMapToChangeTo.length) {
+							BLOCKS[l][h][w] = new MapChanger(
+									Engine.mfo.WFH[currentmap].ReadActionBlocksMapToChangeTo[currentab],
+									Engine.mfo.WFH[currentmap].ReadActionBlocksPlayerX[currentab],
+									Engine.mfo.WFH[currentmap].ReadActionBlocksPlayerY[currentab],
+									Block.RIGHT);
+							currentab += 1;
+						} else {
+							BLOCKS[l][h][w] = new MapChanger(currentmap,0,0,
+									Block.RIGHT);
+						}
+					} else if (ID[l][h][w] == 14) {
+						if (currentab < Engine.mfo.WFH[currentmap].ReadActionBlocksMapToChangeTo.length) {
+							BLOCKS[l][h][w] = new MapChanger(
+									Engine.mfo.WFH[currentmap].ReadActionBlocksMapToChangeTo[currentab],
+									Engine.mfo.WFH[currentmap].ReadActionBlocksPlayerX[currentab],
+									Engine.mfo.WFH[currentmap].ReadActionBlocksPlayerY[currentab],
+									Block.LEFT);
+							currentab += 1;
+						} else {
+							BLOCKS[l][h][w] = new MapChanger(currentmap,0,0,
+									Block.LEFT);
+						}
 					}
 					BLOCKS[l][h][w].l = l + 1;
 					BLOCKS[l][h][w].x = w * Block.w;
@@ -75,6 +168,7 @@ public class MapBuilder {
 				}
 			}
 		}
+		currentab = 0;
 	}
 
 }
--- Realty of Aryenia v2/Tahooey/world/WorldRunner.java
+++ Realty of Aryenia v2/Tahooey/world/WorldRunner.java
@@ -50,6 +50,9 @@ public class WorldRunner {
 				}
 			}
 		}
+		if(Engine.canUpdateMap){
+			Engine.updateMap();
+		}
 	}
 
 }
