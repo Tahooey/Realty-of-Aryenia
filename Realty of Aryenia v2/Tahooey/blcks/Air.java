d13add8d6e6d219e2819795857e82683334fab00
Will <theonlytahooey@hotmail.com>
2013-01-06 13:47:53 -0800

    Debugging with the new isInvisible state

--- Realty of Aryenia v2/Tahooey/blcks/Air.java
+++ Realty of Aryenia v2/Tahooey/blcks/Air.java
@@ -4,11 +4,13 @@ public class Air extends Block{
 	
 	public Air(){
 		isCollidable=false;
+		isInvisible=true;
 		ID=0;
 		imgx=0;
-		imgy=0;
-		imgx2=0;
-		imgy2=0;
+		imgy=8;
+		imgx2=8;
+		imgy2=8;
+		name="Air";
 	}
 
 }
